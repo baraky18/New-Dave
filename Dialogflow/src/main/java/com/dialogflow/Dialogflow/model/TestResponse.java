8045063b5fe4dd258657e0c8a07bd39c462bbc7c
BARAKY <barak.yedaya@gmail.com>
2021-02-02 06:42:42 +0200

    j

---
+++ Dialogflow/src/main/java/com/dialogflow/Dialogflow/model/TestResponse.java
@@ -0,0 +1,15 @@
+package com.dialogflow.Dialogflow.model;
+
+public class TestResponse {
+
+	private String test;
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+	
+}
