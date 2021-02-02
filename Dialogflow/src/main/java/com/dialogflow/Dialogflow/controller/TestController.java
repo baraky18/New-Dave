26b1e5be26be7ca3214d9664954cba81af0066c3
BARAKY <barak.yedaya@gmail.com>
2021-02-02 06:40:35 +0200

    j

--- Dialogflow/src/main/java/com/dialogflow/Dialogflow/controller/TestController.java
+++ Dialogflow/src/main/java/com/dialogflow/Dialogflow/controller/TestController.java
@@ -4,14 +4,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dialogflow.Dialogflow.model.TestResponse;
+
 
 @RestController
 @RequestMapping("/dialogflow")
 public class TestController {
 	
 	@PostMapping("/setintent")
-	public String setIntent(){
-		return "yofi";
+	public TestResponse setIntent(){
+		TestResponse testResponse = new TestResponse();
+		testResponse.setTest("yofi");
+		return testResponse;
 	}
 
 }
