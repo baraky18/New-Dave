dd96c5a9954dcd9b558aba3fb55c9ff15c6185b0
BARAKY <barak.yedaya@gmail.com>
2021-02-04 05:58:07 +0200

    commit

--- Dialogflow/src/main/java/com/dialogflow/Dialogflow/controller/TestController.java
+++ Dialogflow/src/main/java/com/dialogflow/Dialogflow/controller/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.dialogflow.Dialogflow.model.Card;
 import com.dialogflow.Dialogflow.model.FulfillmentMessages;
+import com.dialogflow.Dialogflow.model.TelephonyPlayAudio;
 import com.dialogflow.Dialogflow.model.TestRequest;
 import com.dialogflow.Dialogflow.model.TestResponse;
 import com.dialogflow.Dialogflow.model.Text;
@@ -45,5 +46,26 @@ public class TestController {
 		fulfillmentMessages[0].setCard(card);
 		testResponse.setFulfillmentMessages(fulfillmentMessages);
 		return testResponse;
+	}
+	
+	@PostMapping("/telephonyresponse")
+	public TelephonyPlayAudio telephonyResponse(@RequestBody TestRequest testRequest){
+//		String[] textArr = {"sababa"};
+//		Text text = new Text();
+//		text.setText(textArr);
+//		Card card = new Card();
+//		card.setTitle("audioFile");
+//		card.setSubtitle("Hi man, I understand what your trying to say, but who's gonna believe you");
+//		card.setAudioUri("https://storage.cloud.google.com/new_dave_poc/Hi%20man%2C%20I%20understand%20what%20your%20trying%20to%20say%2C%20but%20who's%20gonna%20believe%20you.mp3");
+//		TestResponse testResponse = new TestResponse();
+//		FulfillmentMessages[] fulfillmentMessages = new FulfillmentMessages[1];
+//		fulfillmentMessages[0] = new FulfillmentMessages();
+//		fulfillmentMessages[0].setText(text);
+//		fulfillmentMessages[0].setCard(card);
+//		testResponse.setFulfillmentMessages(fulfillmentMessages);
+//		return testResponse;
+		TelephonyPlayAudio telephonyPlayAudio = new TelephonyPlayAudio();
+		telephonyPlayAudio.setAudioUri("https://storage.cloud.google.com/new_dave_poc/Hi%20man%2C%20I%20understand%20what%20your%20trying%20to%20say%2C%20but%20who's%20gonna%20believe%20you.mp3");
+		return telephonyPlayAudio;
 	}
 }
---
+++ Dialogflow/src/main/java/com/dialogflow/Dialogflow/model/TelephonyPlayAudio.java
@@ -0,0 +1,14 @@
+package com.dialogflow.Dialogflow.model;
+
+public class TelephonyPlayAudio {
+
+	private String audioUri;
+
+	public String getAudioUri() {
+		return audioUri;
+	}
+
+	public void setAudioUri(String audioUri) {
+		this.audioUri = audioUri;
+	}
+}
